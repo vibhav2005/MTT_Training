c15ef998b5090fcd124acbc2050e682d98c0162b
camo <>
2024-03-19 23:29:04 +0530

    led_on task done

---
+++ Training/tm4c/001_led_on/Debug/main.d
@@ -0,0 +1,9 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/Users/Vibhav\ Verma/OneDrive/Documents/GitHub/MTT_Training/Training/tm4c/001_led_on/tm4c123gh6pge.h
+main.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+
+../main.c: 
+C:/Users/Vibhav\ Verma/OneDrive/Documents/GitHub/MTT_Training/Training/tm4c/001_led_on/tm4c123gh6pge.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
--- Training/tm4c/001_led_on/main.c
+++ Training/tm4c/001_led_on/main.c
@@ -1,9 +1,12 @@
-// include tm4c header file over here
-
-/**
- * main.c
- */
+#include<tm4c123gh6pge.h>
+#include <stdint.h>
+#define true 1
+#define false 0
 int main(void)
 {
-	return 0;
-}
+ SYSCTL_RCGCGPIO_R |=  SYSCTL_RCGCGPIO_R6;
+ GPIO_PORTG_DEN_R  |= 0x000000FF;
+ GPIO_PORTG_DIR_R  |= 0x000000FF;
+ GPIO_PORTG_DATA_R  |= 0x000000FF;
+ while(1){};
+ }
