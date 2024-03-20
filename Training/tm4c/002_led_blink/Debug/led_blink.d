76630f8f52926b4ff9659d30b6aeb01e9b286d71
camo <vibhavvermamtt@gmail.com>
2024-03-20 22:49:05 +0530

    led_blink + optimize done

---
+++ Training/tm4c/002_led_blink/Debug/led_blink.d
@@ -0,0 +1,9 @@
+# FIXED
+
+led_blink.obj: ../led_blink.c
+led_blink.obj: C:/Users/Vibhav\ Verma/OneDrive/Documents/GitHub/MTT_Training/Training/tm4c/002_led_blink/tm4c123gh6pge.h
+led_blink.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h
+
+../led_blink.c: 
+C:/Users/Vibhav\ Verma/OneDrive/Documents/GitHub/MTT_Training/Training/tm4c/002_led_blink/tm4c123gh6pge.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.4.LTS/include/stdint.h: 
--- Training/tm4c/002_led_blink/led_blink.c
+++ Training/tm4c/002_led_blink/led_blink.c
@@ -1,9 +1,23 @@
-
-
-/**
- * main.c
- */
+#include<tm4c123gh6pge.h>
+#include<stdint.h>
+#define true 1
+#define false 0
+static int i,j;
+void delay(){
+    for(i=0;i<900000;i++){};};
 int main(void)
-{
-	return 0;
+{    SYSCTL_RCGCGPIO_R |=  SYSCTL_RCGCGPIO_R6;
+     GPIO_PORTG_DEN_R  |= 0x000000FF;
+     GPIO_PORTG_DIR_R  |= 0x000000FF;
+    while(1){
+        for(j=7;j>=0;j--){
+        GPIO_PORTG_DATA_R = (GPIO_PORTG_DATA_R &(~(0x000000FF))) + (1<<j);
+        delay();
+
+        };
+    };
 }
+
+
+
+
