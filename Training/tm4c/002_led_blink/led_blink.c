c5ca8426e6b419c1b1aa2ee8b311d9ff7b557819
camo <vibhavvermamtt@gmail.com>
2024-03-21 17:06:34 +0530

    all led patterns done

--- Training/tm4c/002_led_blink/led_blink.c
+++ Training/tm4c/002_led_blink/led_blink.c
@@ -4,14 +4,14 @@
 #define false 0
 static int i,j;
 void delay(){
-    for(i=0;i<900000;i++){};};
+    for(i=0;i<300000;i++){};};
 int main(void)
 {    SYSCTL_RCGCGPIO_R |=  SYSCTL_RCGCGPIO_R6;
      GPIO_PORTG_DEN_R  |= 0x000000FF;
      GPIO_PORTG_DIR_R  |= 0x000000FF;
     while(1){
         for(j=7;j>=0;j--){
-        GPIO_PORTG_DATA_R = (GPIO_PORTG_DATA_R &(~(0x000000FF))) + (1<<j);
+        GPIO_PORTG_DATA_R = (GPIO_PORTG_DATA_R &(~(0x000000FF))) + (1<<j) + (1<<(7-j));
         delay();
 
         };
