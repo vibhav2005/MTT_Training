12b469f7bbf475a2908b434d38d34cc1eb4321df
camo <vibhavvermamtt@gmail.com>
2024-03-24 16:43:54 +0530

    ..

--- Training/tm4c/004_led_toggle/main.c
+++ Training/tm4c/004_led_toggle/main.c
@@ -1,34 +1,29 @@
 #include<tm4c123gh6pge.h>
 #include<stdint.h>
-static int B_o = 0,B_n , x;
-
-
-int button_read(){
-    return ((GPIO_PORTG_DATA_R & 0x80)!=0 ?1:0);
+#include<stdbool.h>
+static int B_o = 0,B_n , x;//initialing variables as button old state as B_o and similarly new state
+bool button_read(){
+    return (GPIO_PORTG_DATA_R & 0x80);//creating a function to read button state
 }
 int main(void)
 {
     SYSCTL_RCGCGPIO_R |=  SYSCTL_RCGCGPIO_R6;
-    GPIO_PORTG_DEN_R  |= 0xFF;
-    GPIO_PORTG_DIR_R  |= 0x7F;
-    GPIO_PORTG_DATA_R |= 0x40;
-    GPIO_PORTG_PDR_R  |= 0x80;
-    x = 0;
+    GPIO_PORTG_DEN_R  |= 0xFF;//initializing all pins as digital
+    GPIO_PORTG_DIR_R  |= 0x7F;//initializing some pins as output and one pin as input to take input
+    GPIO_PORTG_DATA_R |= 0x40;//setting one pin high to toggle the input pin using hit switch
+    GPIO_PORTG_PDR_R  |= 0x80;////pulling down the input pin
+    x = 0;//setting x as 0 to read button for the first time
     while(1){
         if(x==0){
         B_n = button_read();
-        x=10000;
+        x=10000;//resetting its value to act as a debouncing delay
         }
-
-        else{
         if(B_o == 0 && B_n == 1 ){
-            GPIO_PORTG_DATA_R ^= 0x1;
-       }
-       B_o = B_n;
+            GPIO_PORTG_DATA_R ^= 0x1;//using xor logic for toggling
+       B_o = B_n; //updating the old state of button
        --x;
       }
-
-     }
+    }
   }
 
 
